4b079ab9055fc853555eb3f8c7463b25de4a578b
Oong <37205723+oong26@users.noreply.github.com>
2020-10-27 20:45:41 +0700

    Tugas 6 - Muhammad Khalil Z.

---
+++ E41192205_MuhammadKhalil/belajar_php/sintax_php_html.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "<html>";
+echo "<head>";
+echo "<title>Belajar PHP</title>";
+echo "</head>";
+echo "<body>";
+echo "<h1>Selamat Datang</h1>";
+echo "</body>";
+echo "</html>";
+
+?>
---
+++ E41192205_MuhammadKhalil/belajar_php/tipe_data.php
@@ -0,0 +1,27 @@
+<?php
+
+//tipe data char (karakter)
+$jenis_kelamin = 'L';
+
+//tipe data string (teks)
+$nama_lengkap = "Muhammad Khalil Zhillullah";
+
+//tipe data integer
+$umur = 20;
+
+//tipe data float
+$berat = 48.3;
+
+//tipe data float
+$tinggi = 182.2;
+
+//tipe data boolean
+$menikah = false;
+
+echo "Nama : $nama_lengkap<br>";
+echo "Jenis Kelamin : $jenis_kelamin<br>";
+echo "Umur : $umur tahun<br>";
+echo "Berat badan : $berat kg<br>";
+echo "Tinggi badan : $tinggi cm<br>";
+echo "Menikah : $menikah";
+?>
